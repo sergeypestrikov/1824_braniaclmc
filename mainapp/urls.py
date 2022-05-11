8de182dbd6be237c46efb9cf6c0d26d1f8279a83
Sergey Pestrikov <sergey_pestrikov@mail.ru>
2022-05-12 00:04:37 +0300

    lesson 2 added static/ for each page

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,10 +6,10 @@ app_name = MainappConfig.name
 
 urlpatterns = [
 
-    path ('contacts/', views.ContactsView.as_view()),
-    path ('courses/', views.CoursesListView.as_view()),
-    path ('docsite/', views.DocSiteView.as_view()),
-    path ('index/', views.IndexView.as_view()),
-    path ('login/', views.LoginView.as_view()),
-    path ('news/', views.NewsView.as_view()),
+    path('contacts/', views.ContactsView.as_view()),
+    path('courses/', views.CoursesListView.as_view()),
+    path('docsite/', views.DocSiteView.as_view()),
+    path('', views.IndexView.as_view()),
+    path('login/', views.LoginView.as_view()),
+    path('news/', views.NewsView.as_view()),
 ]
